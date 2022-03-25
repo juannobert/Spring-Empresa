e8ddc278fa05553ecbf4d8beb30d34ede4c57bdf
Juan Nobert <nobert407@gmail.com>
2022-03-25 09:01:40 -0300

    find by id implemented Part 2

--- src/main/java/com/br/juannobert/empresa/resources/EmployeeResource.java
+++ src/main/java/com/br/juannobert/empresa/resources/EmployeeResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class EmployeeResource {
 	public ResponseEntity<List<Employee>> findAll(){
 		List<Employee> list = service.findAll();
 		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Employee> findById(@PathVariable Long id){
+		Employee employee = service.findById(id);
+		return ResponseEntity.ok(employee);
 	}
 	
 	
